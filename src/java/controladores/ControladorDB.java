3015104facaabfb0a7743ca05d4f4b2b4d32ad6f
joze-net <50345325+joze-net@users.noreply.github.com>
2020-06-02 11:24:19 -0500

    se crearon clases modelo para seguir patron MVC, falta la vista

--- src/java/controladores/ControladorDB.java
+++ src/java/controladores/ControladorDB.java
@@ -18,15 +18,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Resource;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import modelo.GestorProfesor;
 import modelo.Profesor;
 /**
  *
@@ -34,9 +32,8 @@ import modelo.Profesor;
  */
 @WebServlet(name = "ControladorDB", urlPatterns = {"/ControladorDB"})
 public class ControladorDB extends HttpServlet {
-     @Resource(name="jdbc/laboratoriosql")//Aqui hacemos referencia al nombre que le dimos al archivo xml en la etiqueta name
-     private DataSource mipool;//con esta variable es con la que estableceremos la conexion a DDBB
-     private Profesor profe;
+     @Resource(name="jdbc/laboratoriosql")
+   DataSource mipool;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -69,46 +66,12 @@ public class ControladorDB extends HttpServlet {
               
              PrintWriter escribir=response.getWriter();
               response.setContentType("text/html");
-             Connection conexion = null;
-             Statement consultaSql = null;
-             ResultSet rs;
-             List<Profesor> listaProfes=new ArrayList<>();
-             try {
-             //-------------------------------------------------------------------con esas instrucciones se puede tambien establecer la conexion a DDBB
-             /*Context initCtx = new InitialContext(); 
-             Context envCtx = (Context) initCtx.lookup("java:comp/env"); 
-             DataSource ds = (DataSource) envCtx.lookup("jdbc/laboratoriosql");
-             Connection conn = ds.getConnection();
-             */
              
-             //----------------------------------------------
+             GestorProfesor gestorprofesor=new GestorProfesor();
+             List<Profesor> listaprofesor=gestorprofesor.getListaProfesor(mipool);
              
-             conexion=mipool.getConnection();
-             String consulta="select * from profesor";
-             consultaSql=conexion.createStatement();
-             rs=consultaSql.executeQuery(consulta);
-             
-             while(rs.next()){
-                 listaProfes.add(profe=new Profesor(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6)));
-                 
-             } 
-             
-            for(Profesor p:listaProfes){
-                escribir.println(p.getNomProfesor()+"<br> ");
-            }
-         } catch (SQLException ex) {
-             Logger.getLogger(ControladorDB.class.getName()).log(Level.SEVERE, null, ex);
-             escribir.print(ex.getCause());
-             
-             
-         }finally{
-                 try {
-                     conexion.close();
-                     consultaSql.close();
-                 } catch (SQLException ex) {
-                     Logger.getLogger(ControladorDB.class.getName()).log(Level.SEVERE, null, ex);
-                 }
-             }
+             escribir.print(listaprofesor);
+            
            
        
        
---
+++ src/java/modelo/GestorProfesor.java
@@ -0,0 +1,53 @@
+
+package modelo;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+
+
+
+public class GestorProfesor {
+    
+   
+    
+    private List<Profesor> listaProfesor;
+
+    public List<Profesor> getListaProfesor(DataSource mipool) {
+        
+        this.listaProfesor=new ArrayList();
+        
+        try {
+            
+            Connection conexion=mipool.getConnection();//este es el metodo que nos devuelve la conexion
+            Statement sentencia=conexion.createStatement();
+            String consulta="Select * from profesor";
+            
+            ResultSet rs=sentencia.executeQuery(consulta);
+            
+            while(rs.next()){
+                listaProfesor.add(new Profesor(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6)));
+            }//en este while lo que hacemos es rellenar el arraylist con los resulyados obtenidos d la base de datos
+            
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorProfesor.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        
+        return listaProfesor;
+    }
+    
+    
+    
+    
+    
+    
+}
